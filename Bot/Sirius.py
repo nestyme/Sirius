015cd8b74f22952deee8fe2d9a532e8410ec8b55
Chukavin <40797515+Chukavin@users.noreply.github.com>
2018-07-05 13:25:43 +0400

    Vk_api

---
+++ Bot/Sirius.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+import time
+import vk_api
+vk = vk_api.VkApi(login = 'login', password = 'password')
+vk_api.VkApi(token = 'a02d...e83fd') #Авторизоваться как сообщество
+vk.auth()
+
